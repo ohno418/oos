08ae53cfd85515889c432cdabf42087bf95fc7be
taro0 <yutaro.ono.418@gmail.com>
2021-10-07 10:37:52 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+
+use core::panic::PanicInfo;
+
+fn main() {}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+
+// https://os.phil-opp.com/freestanding-rust-binary/#disabling-unwinding
