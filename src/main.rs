532d6efad93a37fb4d841a6d717dae293408e93f
taro0 <yutaro.ono.418@gmail.com>
2021-10-19 18:25:01 +0900

    Print a message when panic

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello, oos!");
-    print!("This is from `print!` macro.");
 
     loop {}
 }
 
 // This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
