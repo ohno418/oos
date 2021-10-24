eab9a3e3a9ad79c560302565efbbf8de326e8af9
taro0 <yutaro.ono.418@gmail.com>
2021-10-24 12:02:22 +0900

    Add test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -9,6 +12,9 @@ mod vga_buffer;
 pub extern "C" fn _start() -> ! {
     println!("Hello, oos!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -17,4 +23,20 @@ pub extern "C" fn _start() -> ! {
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+// Find functions with `#[test_case]` and run them.
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
